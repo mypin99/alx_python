40ef99021e27cbfee2055016c7c3bb4d4e4e2751
ogutuderick <ogutuderick4@gmail.com>
2023-07-23 19:21:39 +0300

    present in both

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,22 +1,3 @@
 #!/usr/bin/python3
-update_dictionary = __import__('2-update_dictionary').update_dictionary
-
-def print_sorted_dictionary(my_dict):
-    """ Print sorted dictionary """
-    keys = sorted(my_dict.keys())
-    for k in keys:
-        print("{}: {}".format(k, my_dict[k]))
-
-a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
-new_dict = update_dictionary(a_dictionary, 'language', "Python")
-print_sorted_dictionary(new_dict)
-print("--")
-print_sorted_dictionary(a_dictionary)
-
-print("--")
-print("--")
-
-new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
-print_sorted_dictionary(new_dict)
-print("--")
-print_sorted_dictionary(a_dictionary)
+def common_elements(set_1, set_2):
+    return set_1.intersection(set_2)
