71db4b05d01c8f8c29fd845d7cbde1e04db6749d
ogutuderick <ogutuderick4@gmail.com>
2023-07-27 21:05:28 +0300

    power

---
+++ python-functions/1-power.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def pow(a, b):
+    result = 1
+    if b >= 0:
+        for _ in range(b):
+            result *= a
+    else:
+        for _ in range(-b):
+            result /= a
+    return result
