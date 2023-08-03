d0fa1028196fcfb77852d90e86b93ac86918d27c
ogutuderick <ogutuderick4@gmail.com>
2023-08-03 09:16:17 +0300

    Exact same object

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,3 +1,24 @@
 #!/usr/bin/python3
-def is_same_class(obj, a_class):
-    return type(obj) is a_class
+a = 1
+print(is_same_class(a, int))      # Output: True
+
+a = True
+print(is_same_class(a, int))      # Output: False
+
+a = 3.14
+print(is_same_class(a, int))      # Output: False
+
+a = True
+print(is_same_class(a, object))   # Output: True
+
+a = None
+print(is_same_class(a, object))   # Output: True
+
+a = None
+print(is_same_class(a, list))     # Output: False
+
+a = [1, 2, 3]
+print(is_same_class(a, list))     # Output: True
+
+a = [1, 2, 3]
+print(is_same_class(a, object))   # Output: False
