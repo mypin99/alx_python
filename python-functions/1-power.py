662426d169c4122c30ab6dc98028cb2f96bffa2c
ogutuderick <ogutuderick4@gmail.com>
2023-08-08 20:07:05 +0300

    a ^ b

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,10 +1,12 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
 def pow(a, b):
-    result = 1
-    if b >= 0:
-        for _ in range(b):
-            result *= a
-    else:
-        for _ in range(-b):
-            result /= a
-    return result
+    return a ** b
+
+
+if __name__ == "__main__":
+    print(pow(2, 2))
+    print(pow(98, 2))
+    print(pow(98, 0))
+    print(pow(100, -2))
+    print(pow(-4, 5))
