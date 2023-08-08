6087543a495e7f8d40d45b894957fa67fed77f71
ogutuderick <ogutuderick4@gmail.com>
2023-08-08 20:37:31 +0300

    Size validation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -2,6 +2,7 @@
 
 """"
     This class represent a Square with a given size.
+
     Attribute:
        _size(int): The size of the square(private attribute).
     """
@@ -10,22 +11,31 @@
 class Square:
     """"
     This class represent a Square with a given size.
+
     Attribute:
        _size(int): The size of the square(private attribute).
     """
 
     def __init__(self, size=0):
         """
-        Initialize a Square object with a given size
+        Initialize a Square object with a optional size
 
         Args:
-            size(int): The size of the Square
+            size(int): The size of the Square(default is 0)
+        Raises:
+            TypeError:If size is not an interger
+            ValueError:If size is less than 0
         """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
         """
         Calculates the area of the square.
+
         Returns:
             int: The area of the square.
         """
@@ -34,6 +44,7 @@ class Square:
     def perimeter(self):
         """
         Calculates the perimeter of the square.
+
         Returns:
             int: The perimeter of the square.
         """
@@ -42,6 +53,7 @@ class Square:
     def get_size(self):
         """
         Gets the size of the square.
+
         Returns:
             int: The size of the square.
         """
@@ -50,12 +62,15 @@ class Square:
     def set_size(self, new_size):
         """
         Sets the size of the square to a new value.
+
         Args:
             new_size (int): The new size to set.
+
         Raises:
-            ValueError: If the new size is negative.
+            TypeError: If the new size is not an integer.
+            ValueError: If the new size is  < 0.
         """
-        if not new_size(int):
+        if not isinstance(new_size, int):
             raise TypeError("size must be an integer")
         if new_size < 0:
             raise ValueError("size must be >= 0")
