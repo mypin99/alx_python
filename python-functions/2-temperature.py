da8167b388b47f8f6fc2c40f86c6787bb4f73e08
ogutuderick <ogutuderick4@gmail.com>
2023-08-08 20:09:19 +0300

    Temperature Converter Function

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
 def convert_to_celsius(fahrenheit):
-    celsius = (fahrenheit - 32) * (5 / 9)
+    celsius = (fahrenheit - 32) * 5 / 9
     return celsius
