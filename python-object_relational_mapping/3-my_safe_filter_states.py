4933d9c9e0d8f77478c8e04de582f727171c2274
ogutuderick <ogutuderick4@gmail.com>
2023-08-28 12:07:42 +0300

    SQL Injection...

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,39 @@
+""" Displays all values in the states table of hbtn_0e_0_usa
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cur = db.cursor()
+
+    query = """
+    SELECT *
+    FROM states
+    WHERE name
+    LIKE BINARY %s
+    ORDER BY id ASC
+    """
+
+    cur.execute(query, (state,))
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
+    
