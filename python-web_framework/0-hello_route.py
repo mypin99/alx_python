cfb9e0caaca5882128df276c2185c6f1844800db
ogutuderick <ogutuderick4@gmail.com>
2023-08-31 22:06:54 +0300

    Hello Flask!

---
+++ python-web_framework/0-hello_route.py
@@ -0,0 +1,14 @@
+"""
+Starting a Flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index(strict_slashes=False):
+    return "Hello HBNB!"
+
+if __name__ == "__main__":
+ app.run(host='0.0.0.0', port=5000)
+ 
