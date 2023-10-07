69eb6de25aad40895cdb58ac29746bd0d995bb27
ogutuderick <ogutuderick4@gmail.com>
2023-10-07 18:52:30 +0300

    Filter states by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,37 +1,14 @@
-""" Displays all values in the states table of hbtn_0e_0_usa
-"""
+#!/usr/bin/python3
+""" Takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument. """
 
-import MySQLdb
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    arg = sys.argv[4]
-
-    db = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
-
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-
-    cur.execute("""
-                SELECT *
-                FROM states
-                WHERE name
-                LIKE BINARY '{}'
-                ORDER BY id ASC
-                """.format(arg))
-
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-
-    cur.close()
-    db.close()
+    cur.execute(
+        "SELECT * FROM `states` WHERE BINARY `name` = '{}' ORDER BY  id ASC".format(sys.argv[4]))
+    [print(state) for state in cur.fetchall()]
     
