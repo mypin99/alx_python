b97a560ae976646e74b277202cc2a222ff2aea9e
ogutuderick <ogutuderick4@gmail.com>
2023-10-07 19:39:42 +0300

    Contains `a`

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -4,22 +4,40 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    # Check if there are enough command-line arguments
+    if len(sys.argv) != 4:
+        print("Usage: python script.py <db_user> <db_password> <db_name>")
+        sys.exit(1)
 
+    # Create a database connection
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True
+    )
+
+    # Create the database tables if they don't exist
     Base.metadata.create_all(engine)
 
+    # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # fetch states that contain letter a
-    states = session.query(State)\
-        .filter(State.name.like('%a%'))\
-        .order_by(State.id)\
-        .all()
+    try:
+        # Query states that contain the letter 'a'
+        states = session.query(State)\
+            .filter(State.name.like('%a%'))\
+            .order_by(State.id)\
+            .all()
 
-    for state in states:
-        print("{}: {}".format(state.id, state.name))
-
-    session.close()
-    
+        # Check if there are any matching records
+        if states:
+            for state in states:
+                print("{}: {}".format(state.id, state.name))
+        else:
+            print("No records found containing 'a'")
+    except Exception as e:
+        print("An error occurred:", str(e))
+    finally:
+        # Close the session
+        session.close()
