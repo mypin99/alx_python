0981248425c46257a13976f798ebacc58d9a5a07
ogutuderick <ogutuderick4@gmail.com>
2023-10-20 08:54:30 +0300

    can you c me now

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+
 def no_c(my_string):
-    new_string = ""
+    result = ""
     for char in my_string:
-        if char.lower() != 'c':
-            new_string += char
-    return new_string
+        if char not in ('c', 'C'):
+            result += char
+    return result
