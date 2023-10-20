d170bab50fa8926675b2281079e98ac12db1fdac
ogutuderick <ogutuderick4@gmail.com>
2023-10-20 08:57:15 +0300

    lists of lists=matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,10 +1,11 @@
 #!/usr/bin/pyton3
 
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
+       for row in matrix:
         for i, num in enumerate(row):
             if i == len(row) - 1:
                 print("{:d}".format(num), end="")
             else:
                 print("{:d}".format(num), end=" ")
-        print()
+        print()  
+        
