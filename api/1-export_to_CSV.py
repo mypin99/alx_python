93c7a2a4ebf484062ab3951cc364295c76369674
ogutuderick <ogutuderick4@gmail.com>
2023-11-04 09:51:14 +0300

    Export to CSV

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,11 +1,6 @@
-"""
-Python script that retrieves employees todos list
-Employees data to be exported in the CSV format
-"""
 import csv
 import requests
 import sys
-
 
 def employees_todo_list(employee_id):
     # Construct the API endpoints based on the provided employee_id
@@ -19,27 +14,22 @@ def employees_todo_list(employee_id):
     todos_data = todos_response.json()
     user_data = user_response.json()
 
-
     employee_name = user_data.get("username")
 
-    alltask_record  = []
+    alltask_record = []
 
     for todo_data in todos_data:
-        alltask_record.append(
-            [
-                employee_id,
-                employee_name,
-                todo_data["completed"],
-                todo_data["title"],
-            ]
-        )
+        alltask_record.append([
+            employee_id,
+            employee_name,
+            todo_data["completed"],
+            todo_data["title"],
+        ])
 
     with open(str(employee_id) + ".csv", "w", encoding="UTF8", newline="") as f:
         writer = csv.writer(f, quoting=csv.QUOTE_ALL)
         writer.writerows(alltask_record)
 
-
-if __name__ == "__main__":
+if __name__ == "__main":
     employee_id = int(sys.argv[1])
     employees_todo_list(employee_id)
-    
